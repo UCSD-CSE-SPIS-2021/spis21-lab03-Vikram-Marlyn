a9f0e4ba4149a9d21a502aaa0cfd4a91252d8b13
Vikram Venkatesh <88351984+vikwaran03@users.noreply.github.com>
2021-08-12 18:42:02 +0000

    Edited irma.py

--- drawLetter_Vikram.py
+++ drawLetter_Vikram.py
@@ -19,12 +19,14 @@ turtle1 = turtle.Turtle()
 draw_V(turtle1)
 
 
+
 turtle2 = turtle.Turtle()
 turtle2.speed(1)
 turtle2.penup()
 turtle2.setpos(-100,0)
 turtle2.pendown()
 draw_V(turtle2)
+
 
 
 
--- irma.py
+++ irma.py
@@ -4,9 +4,7 @@ import csv
 def irma_setup():
     """Creates the Turtle and the Screen with the map background
        and coordinate system set to match latitude and longitude.
-
        :return: a tuple containing the Turtle and the Screen
-
        DO NOT CHANGE THE CODE IN THIS FUNCTION!
     """
     import tkinter
@@ -31,7 +29,7 @@ def irma_setup():
     t = turtle.Turtle()
     wn.register_shape("images/hurricane.gif")
     t.shape("images/hurricane.gif")
-
+    
     return (t, wn, map_bg_img)
 
         
@@ -43,6 +41,11 @@ def irma():
     # t is the turtle, and you will not need the other variables
     (t, wn, map_bg_img) = irma_setup()
 
+    #testing turtle movement
+    #t.color("white")
+    #t.speed(1)
+    #t.forward(-50)
+
     hurricaneFile = "data/irma.csv"
     # The line below is a little magical. It opens the file,
     # with awareness of any errors that might occur.
@@ -51,6 +54,7 @@ def irma():
         # in the file.
         pointreader = csv.reader(csvfile)
 
+        next(csvfile)
         # You'll need to add some code here, before the loop
         # One thing you'll need to figure out how to do is to
         # skip the first line of the file (which is the header).
@@ -66,8 +70,32 @@ def irma():
             # row in the file.
             # Make sure you understand what is happening here.
             # Then, you'll need to change this code
-            print("Date:", row[0], "Time:", row[1])
+            print("Date:", row[0], "Time:", row[1], "Lat:", row[2], "Lon:", row[3], "Wind", row[4] )
 
+            if(int( row[4] ) < 74):
+                t.color("White")
+                t.pensize(1)
+            elif(74 <= int( row[4] ) <= 95):
+                t.color("Blue")
+                t.pensize(3)
+                t.write("1", font = 8)
+            elif(96 <= int( row[4] ) <= 110):
+                t.color("Green")
+                t.pensize(5)
+                t.write("2", font = 8)       
+            elif(111 <= int( row[4] ) <= 129 ):
+                t.color("Yellow")
+                t.pensize(7)
+                t.write("3", font = 8)
+            elif(130 <= int( row[4] ) <= 156):
+                t.color("Orange")
+                t.pensize(9)
+                t.write("4", font = 8)
+            elif(int( row[4] ) >= 157):
+                t.color("Red")
+                t.pensize(12)
+                t.write("5", font = 8)
+            t.setpos(float( row[3] ), float( row[2] ))
 
 
     # Hack to make sure a reference to the background image stays around
