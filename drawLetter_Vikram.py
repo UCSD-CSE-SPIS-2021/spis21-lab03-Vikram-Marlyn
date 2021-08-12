6a322ca50908d1994613dc36992ad9ba63904c72
VikramVenkatesh <>
2021-08-12 05:47:18 +0000

    Added drawLetter function

---
+++ drawLetter_Vikram.py
@@ -0,0 +1,34 @@
+# Vikram - Draw my first initial
+
+import turtle
+
+def draw_V(the_turtle): 
+
+  """ Draw my firt initial """
+
+  the_turtle.speed(1)
+  the_turtle.left(115)
+  the_turtle.forward(100)
+  the_turtle.backward(100)
+  the_turtle.right(55)
+  the_turtle.forward(100)
+  
+  
+
+turtle1 = turtle.Turtle()
+draw_V(turtle1)
+
+
+turtle2 = turtle.Turtle()
+turtle2.speed(1)
+turtle2.penup()
+turtle2.setpos(-100,0)
+turtle2.pendown()
+draw_V(turtle2)
+
+
+
+
+
+
+
